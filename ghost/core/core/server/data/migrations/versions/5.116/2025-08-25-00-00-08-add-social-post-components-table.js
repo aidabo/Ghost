6f0ec80740ffd62321410221822aafee7d2ca026
aidabo <aidabo@janabiz.com>
2025-10-31 18:29:53 +0900

    change post_components, name->type, add published_at

--- ghost/core/core/server/data/migrations/versions/5.116/2025-08-25-00-00-08-add-social-post-components-table.js
+++ ghost/core/core/server/data/migrations/versions/5.116/2025-08-25-00-00-08-add-social-post-components-table.js
@@ -2,7 +2,7 @@ const {addTable} = require('../../utils');
 
 module.exports = addTable('post_components', {
     id: {type: 'string', maxlength: 24, nullable: false, primary: true},
-    name: {type: 'string', maxlength: 60, nullable: false, index: true},
+    type: {type: 'string', maxlength: 60, nullable: false, index: true},
     title: {type: 'string', maxlength: 191, nullable: false},
     excerpt: {type: 'string', maxlength: 500, nullable: true},
     image: {type: 'string', maxlength: 500, nullable: true},
@@ -10,6 +10,7 @@ module.exports = addTable('post_components', {
     layout: {type: 'text', maxlength: 1000000000, nullable: true},
     tag: {type: 'string', maxlength: 32, nullable: true, index: true},
     status: {type: 'string', maxlength: 50, nullable: false, defaultTo: 'draft', validations: {isIn: [['published', 'draft']]}},
+    published_at: {type: 'dateTime', nullable: true},
     created_at: {type: 'dateTime', nullable: false},
     created_by: {type: 'string', maxlength: 24, nullable: false},
     updated_at: {type: 'dateTime', nullable: false},
