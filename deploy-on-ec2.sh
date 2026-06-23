5e7a40db23e5afad9631ad675851ec7cea62c6c6
aidabo <aidabo@janabiz.com>
2026-06-23 23:36:22 +0900

    fix docker build file to add lib folder path in Dockerfile

--- deploy-on-ec2.sh
+++ deploy-on-ec2.sh
@@ -5,7 +5,8 @@ S3_BUCKET="60-legacy"
 S3_FOLDER="deploy-image"
 AWS_REGION="ap-northeast-1"
 IMAGE_NAME="ghost"
-VERSION="5.116.2-next-r6"
+# Update this version as needed
+VERSION="5.116.2-next-r6b" 
 
 # Download from S3
 aws s3 cp s3://$S3_BUCKET/$S3_FOLDER/$IMAGE_NAME-$VERSION.tar . --region $AWS_REGION
--- ghost/core/core/server/data/migrations/versions/5.116/2026-06-11-00-00-01-add-person-media-updated-at.js
+++
@@ -1,6 +0,0 @@
-const {createAddColumnMigration} = require('../../utils');
-
-module.exports = createAddColumnMigration('person_media', 'updated_at', {
-    type: 'dateTime',
-    nullable: true
-});
